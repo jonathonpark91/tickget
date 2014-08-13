313b56de827dcf03ff177cc0cbd0abeb76690f6c
jonathonpark91 <jonathonpark@awesome>
2014-08-12 19:53:14 -0700

    made new ticket work and going for after show

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -9,14 +9,23 @@ class TicketsController < ApplicationController
 
 	def create 
 		@ticket = Ticket.new(ticket_params)
+
+		if @ticket.save
+			@tickets = Ticket.all
+			render action: 'index'
+    	else
+	        render action: 'new'
+	    end
 	end
 
 	def show
 		@ticket = Ticket.find(params[:id])
 	end
+
+	private
+	def ticket_params
+		params.require(:ticket).permit(:concert_name, :date_time, :venue, :price, :quantity, :trade_value)
+	end
 end
 
-private
-	def store_params
-		params.require(:ticket).permit(:concert_name)
-	end
+
